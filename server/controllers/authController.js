06f04a5aa2d5976a9a292397749c79b72cf6a978
Shozab <shozabali06@gmail.com>
2024-12-26 20:38:14 +0500

    Refactor password reset flow: implement OTP verification and email templates for reset and verification

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,10 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 import transporter from "../config/nodemailer.js";
+import {
+  EMAIL_VERIFY_TEMPLATE,
+  PASSWORD_RESET_TEMPLATE,
+} from "../config/emailTemplates.js";
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -155,7 +159,11 @@ export const sendVerifyOtp = async (req, res) => {
       from: process.env.SENDER_EMAIL,
       to: user.email,
       subject: "Account Verification OTP",
-      text: `Hi ${user.name}, \nYour OTP is ${otp}\nVerify your account using this OTP`,
+      // text: `Hi ${user.name}, \nYour OTP is ${otp}\nVerify your account using this OTP`,
+      html: EMAIL_VERIFY_TEMPLATE.replace("{{otp}}", otp).replace(
+        "{{email}}",
+        user.email
+      ),
     };
 
     await transporter.sendMail(mailOptions);
@@ -265,7 +273,11 @@ export const sendResetOtp = async (req, res) => {
       from: process.env.SENDER_EMAIL,
       to: user.email,
       subject: "Password Reset OTP",
-      text: `Hi ${user.name}, \nYour OTP is ${otp}\nUse this OTP to reset your password`,
+      // text: `Hi ${user.name}, \nYour OTP is ${otp}\nUse this OTP to reset your password`,
+      html: PASSWORD_RESET_TEMPLATE.replace("{{otp}}", otp).replace(
+        "{{email}}",
+        user.email
+      ),
     };
 
     await transporter.sendMail(mailOptions);
