cbdc937d3b816ccce0dd4ece63c16ffe54d792b8
Shozab <shozabali06@gmail.com>
2024-12-27 08:02:55 +0500

    Add GitHub logo to Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -42,37 +42,43 @@ const Navbar = () => {
   return (
     <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
       <img src={assets.logo} alt="logo" className="w-28 sm:w-32" />
-      {userData ? (
-        <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group">
-          {userData.name[0].toUpperCase()}
-          <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
-            <ul className="list-none m-0 p-2 bg-gray-100 text-sm rounded">
-              {!userData.isAccountVerified && (
-                <li
-                  onClick={sendVerificationOtp}
-                  className="py-1 px-2 hover:bg-gray-200 cursor-pointer rounded"
-                >
-                  Verify Email
-                </li>
-              )}
+      <div className="flex items-center gap-3">
+        <a href="https://github.com/shozabali06/User-Authentication-MERN" target="_blank">
+          <img className="w-10" src={assets.github_logo} alt="github_logo" />
+        </a>
 
-              <li
-                onClick={logout}
-                className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10 rounded"
-              >
-                Logout
-              </li>
-            </ul>
+        {userData ? (
+          <div className="w-10 h-10 flex justify-center items-center rounded-full bg-black text-white relative group">
+            {userData.name[0].toUpperCase()}
+            <div className="absolute hidden group-hover:block top-2 right-0 z-10 text-black rounded pt-10">
+              <ul className="list-none m-0 p-2 bg-gray-100 text-sm rounded">
+                {!userData.isAccountVerified && (
+                  <li
+                    onClick={sendVerificationOtp}
+                    className="py-1 px-2 hover:bg-gray-200 cursor-pointer rounded"
+                  >
+                    Verify Email
+                  </li>
+                )}
+
+                <li
+                  onClick={logout}
+                  className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10 rounded"
+                >
+                  Logout
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      ) : (
-        <button
-          onClick={() => navigate("/login")}
-          className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all"
-        >
-          Login <img src={assets.arrow_icon} alt="arrow_icon" />
-        </button>
-      )}
+        ) : (
+          <button
+            onClick={() => navigate("/login")}
+            className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all"
+          >
+            Login <img src={assets.arrow_icon} alt="arrow_icon" />
+          </button>
+        )}
+      </div>
     </div>
   );
 };
